620006a56e0642985d7feb3d325d1365cfd0841d
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-01-18 13:28:56 +0530

    First commit.

---
+++ finance/src/main/java/com/finance/finance/App.java
@@ -0,0 +1,14 @@
+package com.finance.finance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("First commit.");
+    }
+}
